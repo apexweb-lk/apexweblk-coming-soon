c82b5a6b40823dba55b00f191350ccf407506c94
hirusha-adi <hirushaadi@gmail.com>
2024-09-16 19:24:29 +0530

    contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Typewriter from 'typewriter-effect';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <p className="small-text">
+        For an Exceptional Web Experience!
+      </p>
+      <h1>
+        <Typewriter
+          options={{
+            strings: ['Apex Web', 'Coming Soon!'],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </h1>
+      <p className="small-text">
+        Contact us at <br></br>
+        <a href="mailto:info@apexweb.lk">
+          <code>info@apexweb.lk</code>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        &nbsp;/&nbsp;
+        <a href="tel:+94777662700">
+          <code>+94777662700</code>
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
       </p>
     </>
   )
